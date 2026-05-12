c1715bad3a0435a2238650bcf0a2f2a0fd918264
medison0001 <23665281+medison0001@users.noreply.github.com>
2026-05-12 13:31:22 -0500

    Refactor project structure and update styles

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,17 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
-
 @Component({
-  selector: 'app-root',
+  selector: 'app-labs',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css'
 })
-export class AppComponent {
-  title = 'todo-app';
+export class LabsComponent {
   welcome = 'Biendvenido a mi primera aplicación con Angular';
   tasks = [
     'Instalar Angular CLI',
