b49668aedc4a55c6f23c8044798d1aca4b231211
medison0001 <23665281+medison0001@users.noreply.github.com>
2026-05-12 18:56:38 -0500

    Add Task model and implement task management in Home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { Task } from '../../models/taks.model';
+import { map } from 'rxjs';
+
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -9,21 +12,57 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  tasks = signal([
-    'Instalar Angular CLI',
-    'Crear un nuevo proyecto',
-    'Desarrollar la aplicación',
-    'Probar la aplicación',
-    'Desplegar la aplicación'
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: 'Instalar Angular CLI',
+      completed: false
+    },
+    {
+      id: Date.now() + 1,
+      title: 'Crear un nuevo proyecto',
+      completed: false
+    },
+    {
+      id: Date.now() + 2,
+      title: 'Desarrollar la aplicación',
+      completed: false
+    },
+    {
+      id: Date.now() + 3,
+      title: 'Probar la aplicación',
+      completed: false
+    },
+    {
+      id: Date.now() + 4,
+      title: 'Desplegar la aplicación',
+      completed: false
+    }
   ]);
 
   changeHandler(event: Event){
     const input = event.target as HTMLInputElement;
     const newTask = input.value;
+    this.addTask(newTask);
+  }
+
+  addTask(title: string){
+    const newTask = { id: Date.now(), title, completed: false };
     this.tasks.update(tasks => [...tasks, newTask]);
   }
 
   deleteTask(index: number){
     this.tasks.update(tasks => tasks.filter((_, i) => i !== index));
+  }
+
+  updateTask(index: number){
+    this.tasks.update(tasks => {
+      return tasks.map(task => {
+        if (tasks.indexOf(task) === index) {
+          return { ...task, completed: !task.completed };
+        }
+        return task;
+      });
+    });
   }
 }
